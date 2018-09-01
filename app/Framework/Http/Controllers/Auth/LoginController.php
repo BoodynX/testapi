1fdd6a1448849c69c58195634ef0863afa728466
Tomasz Budny <budny.tomasz@gmail.com>
2018-09-01 14:36:18 +0200

    refactor: framework isolation

--- app/Framework/Http/Controllers/Auth/LoginController.php
+++ app/Framework/Http/Controllers/Auth/LoginController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Framework\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use App\Framework\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
